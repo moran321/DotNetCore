092f3a43b448735cea6fa1d16b46f7447a05290b
Moran Ankori <morana@codevalue.net>
2018-09-09 03:00:00 -0400

    models

--- vega/Startup.cs
+++ vega/Startup.cs
@@ -7,56 +7,50 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-namespace vega
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+using vega.Persistence;
+using Microsoft.EntityFrameworkCore;
+namespace vega {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-       //'IServiceCollection' is the container of all the dependencies of the application
-        public void ConfigureServices(IServiceCollection services)
-        {
+        //'IServiceCollection' is the container of all the dependencies of the application
+        public void ConfigureServices (IServiceCollection services) {
             //for dependency injection
             //we need to add the services we use in our controllers
             //example: services.AddTransient<IRepository, Repository>(); -> inject the implementation of the interface
 
-            services.AddMvc();
+          //  services.AddDbContext<VegaDbContext> (options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddDbContext<VegaDbContext> (options => options.UseSqlServer("server=localhost; database=vega; integrated security=sspi"));
+         //"Data Source=.;Initial Catalog=Benchmark;Persist Security Info=True;User ID=****;Password=****"
+            services.AddMvc ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+                app.UseWebpackDevMiddleware (new WebpackDevMiddlewareOptions {
                     HotModuleReplacement = true //if we change the client files we dont need to reload the page
                 });
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
             }
 
-            app.UseStaticFiles(); //search for images etc.
+            app.UseStaticFiles (); //search for images etc.
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
+            app.UseMvc (routes => {
+                routes.MapRoute (
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
 
-                routes.MapSpaFallbackRoute(
+                routes.MapSpaFallbackRoute (
                     name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" });
+                    defaults : new { controller = "Home", action = "Index" });
             });
         }
     }
